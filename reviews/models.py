d518e359ce56f330c487bb74b352594983af0c02
jisoo park <qkr1wltn@naver.com>
2022-01-25 00:03:39 +0900

    review 1

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from user.models import Member
+
+class Review(models.Model):
+  member_id = models.ForeignKey(
+    Member, on_delete = models.SET_NULL, null=True, db_column = 'member_id')
+  date = models.DateTimeField()
+  store_name = models.CharField(max_length=50)
+  title = models.CharField(max_length=50)
+  body = models.TextField()
+
+  class Meta:
+    db_talbe = 'review'
+    managed = False
