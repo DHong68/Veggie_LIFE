00ae0cd4752e00dbca3023174f13d75ab055f110
KimJY37 <gmlwlswldbs@naver.com>
2022-01-25 15:50:42 +0900

    회원가입 유효성 검사

--- user/urls.py
+++ user/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
-    path('check_id/', views.check_id, name='check_id'),
+    # path('delete/', views.delete, name='delete'),
+    # path('check_id/', views.check_id, name='check_id'),
 
 
 ]
