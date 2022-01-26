44769250f345771eed2589b93184d7154bb170e1
jisoo park <qkr1wltn@naver.com>
2022-01-26 10:40:34 +0900

    review 수정

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'reviews'
 
 urlpatterns = [
-    # path('write/', views.write, name = 'write'),
-    path('list/', views.list, name='list'),
-    path('insert/', views.insert, name='insert'),
+    path('write/', views.write, name = 'write'),
+    # path('list/', views.list, name='list'),
+    # path('insert/', views.insert, name='insert'),
     # path('delete/<int:id>', views.delete, name = 'delete'),
     # path('update/<int:id>/', views.update, name = 'update'),
 
