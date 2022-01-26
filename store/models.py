6374ecc614ad6a1a9e36abfba8598ee26af6d241
jisoo park <qkr1wltn@naver.com>
2022-01-26 11:01:21 +0900

    merge

--- store/models.py
+++ store/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.db.models.fields import CharField, TextField
 
 class Store(models.Model):
-    name = CharField(max_length=100)
-    type = CharField(max_length=10)
-    phone = CharField(max_length=20)
-    gu = CharField(max_length=10)
-    address = TextField()
-    menu = TextField()
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=10)
+    phone = models.CharField(max_length=20)
+    gu = models.CharField(max_length=10)
+    address = models.TextField()
+    menu = models.TextField()
 
     class Meta:
         db_table = 'store'
