b303aae2ebbfb372f84b9b6e262770d8101e4563
KimJY37 <gmlwlswldbs@naver.com>
2022-01-26 10:57:33 +0900

    마이페이지 + 작성글

--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponse
 from .models import User
+from reviews.models import Review
 from .forms import SignupForm
 
 def home(request):
@@ -48,7 +49,6 @@ def logout(request):
      request.session.flush()
      return redirect('/')
 
-
 def delete(request):
      user_id = request.session['user_id']
      delete_user = User.objects.get(user_id = user_id)
@@ -59,46 +59,30 @@ def delete(request):
 def mypage(request):
      user_id = request.session['user_id']
      user = User.objects.get(user_id = user_id)
+     posts = Review.objects.filter(member_id = user.id).values()
+     if len(posts) >= 5:
+          posts = posts[0:5]
      context = {}
+     context['posts'] = posts
      if user:
           context['user'] = user
           return render(request, 'user/mypage.html', context)
      else:
           return redirect('/')
 
-# def update(request, user_id):
-
-#      if request.method == 'POST':
-#           form = SignupForm(request.POST)
-#           re_password = request.POST.get('re_password')
-#           context = {}
-#           if form.is_valid():
-#                signupform = form.save(commit=False)
-#                if signupform.password != re_password:
-#                     context['error'] = '비밀번호가 일치하지 않습니다.' 
-#                     return render(request, "user/signup.html", context)
-#                signupform.save()
-#                return redirect('/user/login')
-#      else:
-#           form = SignupForm()
-#      return render(request, 'user/signup.html', {'form': form})
-
-# from .forms import CustomUserChangeForm
-# from django.contrib.auth.decorators import login_required
-
-# @login_required
-# def update(request):
-#     if request.method == 'POST':
-#           user_change_form = CustomUserChangeForm(request.POST, instance=request.user)
-#           re_password = request.POST.get('re_password')
-#           context = {}
-#           if user_change_form.is_valid():
-#                updateform = user_change_form.save(commit=False)
-#                if updateform.password != re_password:
-#                     context['error'] = '비밀번호가 일치하지 않습니다.' 
-#                     return render(request, "user/update.html", context)
-#                updateform.save()
-#                return redirect('/user/login')
-#     else:
-#           user_change_form = CustomUserChangeForm(instance = request.user)
-#           return render(request, 'user/update.html', {'user_change_form':user_change_form})
+def update(request, user_id):
+     user = get_object_or_404(User, user_id = user_id)
+     if request.method == 'POST':
+          form = SignupForm(request.POST, instance=user)
+          re_password = request.POST.get('re_password')
+          context = {}
+          if form.is_valid():
+               signupform = form.save(commit=False)
+               if signupform.password != re_password:
+                    context['error'] = '비밀번호가 일치하지 않습니다.' 
+                    return render(request, "user/signup.html", context)
+               signupform.save()
+               return redirect('/user/login')
+     else:
+          form = SignupForm(instance=user)
+     return render(request, 'user/update.html', {'form': form})
