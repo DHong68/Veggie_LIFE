6d60312d1f2a5f29c373814b51dc58c437908f04
ehddbs1231 <ehddbs1231@naver.com>
2022-01-27 15:16:56 +0900

    review 파일 다운로드

--- reviews/urls.py
+++ reviews/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('delete/<int:id>/', views.delete, name = 'delete'),
     path('update/<int:id>/', views.update, name = 'update'),
     path('details/', views.details, name='details'),
+    path('download/<int:id>/', views.download, name='download'),
 ]
